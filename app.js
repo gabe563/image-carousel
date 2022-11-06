8f8534e3482f71332d589db3853d44390771f4f3
FOXYSAMU <reefebghr@gmail.com>
2022-11-06 15:09:29 -0500

    Finished main functionality!

--- app.js
+++ app.js
@@ -29,57 +29,64 @@ function currentSlide() {
   });
 }
 
+let imageIndex = 1;
+
 function changeCurrImage(dot) {
-  const imagesSrc = [
-    './assets/cat1.png',
-    './assets/cat2.jpeg',
-    './assets/cat3.jpeg',
-    './assets/cat4.jpg',
-    './assets/cat5.jpeg',
-  ];
-
-  function checkCurrSelected() {
-    let currImage;
-    switch (dot.dataset.id) {
-      case '1':
-        currImage = imagesSrc[0];
-        break;
-      case '2':
-        currImage = imagesSrc[1];
-        break;
-      case '3':
-        currImage = imagesSrc[2];
-        break;
-      case '4':
-        currImage = imagesSrc[3];
-        break;
-      case '5':
-        currImage = imagesSrc[4];
-        break;
+  function currImage() {
+    if (!dot.classList.contains('addCurrent')) {
+      showImage((imageIndex = +dot.dataset.id));
     }
-    return currImage;
+  }
+  currImage();
+}
+
+function changeImgOnClick() {
+  const leftBtn = document.querySelector('.left');
+  const rightBtn = document.querySelector('.right');
+
+  leftBtn.addEventListener('click', () => {
+    showImage((imageIndex += -1));
+  });
+
+  rightBtn.addEventListener('click', () => {
+    showImage((imageIndex += 1));
+  });
+}
+
+function showImage(id) {
+  const slides = document.querySelectorAll('.image');
+  const dots = document.querySelectorAll('.dot');
+
+  if (id > slides.length) {
+    imageIndex = 1;
   }
 
-  if (!dot.classList.contains('addCurrent')) {
-    let savedImg = checkCurrSelected();
-    const mainImg = document.querySelector('.slideshow img');
-    mainImg.classList.add('fadeOut');
-    setTimeout(() => {
-      mainImg.src = savedImg;
-    }, 500);
-    document.querySelector('.fadeOut').addEventListener('animationend', () => {
-      mainImg.style.opacity = 0;
-      mainImg.classList.remove('fadeOut');
-      mainImg.classList.add('fadeIn');
-      document
-        .querySelector('.fadeIn')
-        .addEventListener('animationend', (e) => {
-          e.stopPropagation();
-          mainImg.style = '';
-          mainImg.classList.remove('fadeIn');
-        });
-    });
+  if (id < 1) {
+    imageIndex = slides.length;
   }
+
+  slides.forEach((slide) => {
+    if (slide.classList.contains('current')) {
+      slide.classList.remove('current');
+    }
+  });
+
+  dots.forEach((dot) => {
+    if (dot.classList.contains('addCurrent')) {
+      if (!dot.classList.contains('removeCurrent')) {
+        dot.classList.add('removeCurrent');
+        dot.addEventListener('animationend', () => {
+          dot.classList.remove('removeCurrent');
+        });
+      }
+      dot.classList.remove('addCurrent');
+    }
+  });
+
+  slides[imageIndex - 1].classList.add('current');
+  dots[imageIndex - 1].classList.add('addCurrent');
 }
+
+changeImgOnClick();
 
 currentSlide();
