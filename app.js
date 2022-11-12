a58b6cacd1dadf04c9bbda3ef43680461fcfba8f
FOXYSAMU <reefebghr@gmail.com>
2022-11-12 12:42:49 -0500

    Added 5 sec cooldown (img)

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ function currentSlide() {
 }
 
 let imageIndex = 1;
+let cooldownImage = 0;
+window.onload = () => {
+  imageCooldown();
+};
 
 function changeCurrImage(dot) {
   function currImage() {
@@ -41,20 +45,31 @@ function changeCurrImage(dot) {
 }
 
 function changeImgOnClick() {
+  let counter = 0;
   const leftBtn = document.querySelector('.left');
   const rightBtn = document.querySelector('.right');
 
   leftBtn.addEventListener('click', () => {
-    showImage((imageIndex += -1));
+    counter = counter + 1;
+    if (counter !== 10) {
+      showImage((imageIndex += -1));
+    }
   });
 
   rightBtn.addEventListener('click', () => {
-    showImage((imageIndex += 1));
+    counter = counter + 1;
+
+    if (counter !== 10) {
+      showImage((imageIndex += 1));
+    } else {
+      delete counter;
+      return;
+    }
   });
 }
 
 function showImage(id) {
-  const slides = document.querySelectorAll('.image');
+  const slides = document.querySelectorAll('.slideshow > img');
   const dots = document.querySelectorAll('.dot');
 
   if (id > slides.length) {
@@ -66,8 +81,30 @@ function showImage(id) {
   }
 
   slides.forEach((slide) => {
-    if (slide.classList.contains('current')) {
-      slide.classList.remove('current');
+    // if (slide.classList.contains('fadeIn')) {
+    //   slide.classList.add('fadeOut');
+    //   document
+    //     .querySelector('.fadeOut')
+    //     .addEventListener('animationend', () => {
+    //       slide.classList.add('hidden');
+    //       slide.classList.remove('fadeOut');
+    //       slide.classList.remove('fadeIn');
+
+    //       slides[imageIndex - 1].classList.add('fadeIn');
+    //       slides[imageIndex - 1].classList.remove('hidden');
+    //     });
+    // }
+    if (
+      !slide.classList.contains('hidden') ||
+      !slide.classList.contains('visual')
+    ) {
+      slide.classList.add('visual');
+      slide.addEventListener('transitionend', () => {
+        slide.classList.add('hidden');
+        slide.offsetWidth;
+        slides[imageIndex - 1].classList.remove('hidden');
+        slides[imageIndex - 1].classList.remove('visual');
+      });
     }
   });
 
@@ -83,8 +120,52 @@ function showImage(id) {
     }
   });
 
-  slides[imageIndex - 1].classList.add('current');
   dots[imageIndex - 1].classList.add('addCurrent');
+  cooldownImage = imageIndex;
+}
+
+function imageCooldown() {
+  const slides = document.querySelectorAll('.slideshow > img');
+  const dots = document.querySelectorAll('.dot');
+
+  cooldownImage++;
+  if (cooldownImage > slides.length) {
+    cooldownImage = 1;
+  }
+  imageIndex = cooldownImage;
+
+  slides.forEach((slide) => {
+    if (
+      !slide.classList.contains('hidden') ||
+      !slide.classList.contains('visual')
+    ) {
+      slide.classList.add('visual');
+      slide.addEventListener('transitionend', () => {
+        slide.classList.add('hidden');
+        slide.offsetWidth;
+        slides[cooldownImage - 1].classList.remove('hidden');
+        setTimeout(() => {
+          slides[cooldownImage - 1].classList.remove('visual');
+        }, 20);
+      });
+    }
+  });
+
+  dots.forEach((dot) => {
+    if (dot.classList.contains('addCurrent')) {
+      if (!dot.classList.contains('removeCurrent')) {
+        dot.classList.add('removeCurrent');
+        dot.addEventListener('animationend', () => {
+          dot.classList.remove('removeCurrent');
+        });
+      }
+      dot.classList.remove('addCurrent');
+    }
+  });
+
+  dots[cooldownImage - 1].classList.add('addCurrent');
+
+  setTimeout(imageCooldown, 5000);
 }
 
 changeImgOnClick();
